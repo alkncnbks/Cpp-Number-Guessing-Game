75a790ce5b5a91c3601b3bbff15357e740359104
alkncnbks <70758847+alkncnbks@users.noreply.github.com>
2022-12-14 16:11:39 +0300

    Add files via upload

--- guess.cpp
+++ guess.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main() {
 	
 	int guess;
-	int answer = 32;
+	int answer = 35;
 	
 	cout<<"Welcome to our number guessing game"<<endl;
 	cout<<"Rules are simple,just guess a number let's say our number is greater or less than that number."<<endl;
